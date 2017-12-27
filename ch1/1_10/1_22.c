977584f5c333bb79cd5baddd72b01b37f4b4e316
heathkang <heath.kang@foxmail.com>
2017-12-27 21:27:13 +0800

    fix 1_22 bug

--- ch1/1_10/1_22.c
+++ ch1/1_10/1_22.c
@@ -7,35 +7,59 @@ before the specified column.
 #include<stdio.h>
 
 #define numofline 8
+#define MAXLINE 1000
+
+int getline(char s[], int len);
+
+void print(char s[], int len);
 
 main()
 {
-    int c;
-    int num = 0;
-    int cnum = 0;
+    char line[MAXLINE];
+    int len;
 
-    while((c=getchar())!= EOF){
-        ++num;
-        
-        if (num==numofline){
-            if(c!=' '){
-                putchar(c);
-                putchar('\n');
-                num = 0;
-            }
-        } else if(num>numofline) {
-            if(c!=' '){
-                putchar('\n');
-                putchar(c);
-                num = 1;
-            }
-        } else {
-            putchar(c);
-        }
-        
-    }
+    len = getline(line, MAXLINE);
+    print(line, len);
 }
 
+int getline(char s[], int lim)
+{
+    int c, i;
+    for(i=0; i < lim - 1 && (c =getchar())!=EOF && c!='\n'; ++i)
+        s[i] = c;
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
 
-
+void print(char s[], int len)
+{
+    int i;
+    int num=0;
+    for(i=0;i<len;++i){
+        ++num;
+        if (num==numofline){             //1 num == numline 
+            if(s[i+1]!=' '){             // 1.1 next is not blank           
+                 putchar(s[i]); 
+                 putchar('\n');
+                 num = 0;
+             }else{                      // 1.2 next is blank
+                 putchar(s[i]);  
+             }
+         } else if(num>numofline) {      // 1.2 num > num line 
+            if(s[i+1]!=' '){             // 1.2.1 next is not blank
+                putchar(s[i]);
+                putchar('\n'); 
+                num = 0;
+            }else{                       // 1.2.2 next is blank
+                putchar(s[i]);
+            }
+         } else {
+            putchar(s[i]);
+        }
+    }
+}
 
