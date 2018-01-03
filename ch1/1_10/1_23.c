07a60ac12b84ef2bb4c29288dcc702b591200585
heathkang <heath.kang@foxmail.com>
2018-01-03 20:41:08 +0800

    1_24 remain empty

--- ch1/1_10/1_23.c
+++ ch1/1_10/1_23.c
@@ -9,8 +9,8 @@ handle quoted strings and character constants properly. C comments don't nest.
 int getline(char s[]);
 void eliminate_comment(char s[], int len);
 
-int get_comment_state(char now, char next, int last_comment_state);
-int get_qutoed_state(char s, int last_qutoed_state);
+int get_comment_state(char now, char next, int last_comment_state, int qutoed_state);
+int get_qutoed_state(char now, int last_qutoed_state);
 
 
 main()
@@ -38,44 +38,49 @@ void eliminate_comment(char s[], int len)
     int i;
     int comment_state = 0;
     int qutoed_state = 0;
+
     for(i=0;i<len;i++){
-        comment_state = get_comment_state(s[i], s[i+1], comment_state);
+        
+
         qutoed_state = get_qutoed_state(s[i], qutoed_state);
 
-        if (qutoed_state==0 && comment_state == 0){
+        comment_state = get_comment_state(s[i], s[i+1], comment_state, qutoed_state);
+
+        if (comment_state == 0){
             putchar(s[i]);
-        } else if (qutoed_state==1 && comment_state==0){
-            putchar(s[i]);
-        } else if (qutoed_state==0 && comment_state==1){
-            
-        } else if(qutoed_state==1 && comment_state==1){
-            putchar(s[i]);
-        }
+        } else {
+        } 
     }
 }
 
-int get_comment_state(char now, char next, int last_comment_state)
+int get_comment_state(char now, char next, int last_comment_state, int qutoed_state)
 {
     int comment_state = last_comment_state;
 
-    if (now=='/' && next == '*'){
-        comment_state = 1;
-    } else if (now=='*' && next=='/'){
-        comment_state = 0;
-    } else if (now=='/'&& next=='/'){
-        comment_state = 1;
-    } else if (last_comment_state==1 && now=='\n'){
-        comment_state = 0;
+    if (qutoed_state == 1){
+        // in qutoed comment don't work;
+    } else if ((qutoed_state==0)&&(now=='"'|| now=='\'')){
+        // in qutoed comment don't work;
+    } else {
+        if (now=='/' && next == '*'){
+            comment_state = 1;
+        } else if (now=='*' && next=='/'){
+            comment_state = 1;
+        } else if (now=='/'&& next=='/'){
+            comment_state = 1;
+        } else if (last_comment_state==1 && now=='\n'){
+            comment_state = 0;
+        }
     }
     return comment_state;
 }
 
-int get_qutoed_state(char s, int last_qutoed_state)
+int get_qutoed_state(char now, int last_qutoed_state)
 {
     int qutoed_state = last_qutoed_state;
-    if ((s=='"'|| s=='\'' )&& last_qutoed_state == 1){
+    if ((now=='"'|| now=='\'' )&& last_qutoed_state == 1){
         qutoed_state = 0;
-    } else if ((s=='"'|| s=='\'' )&& last_qutoed_state == 0){
+    } else if ((now=='"'|| now=='\'' )&& last_qutoed_state == 0){
         qutoed_state = 1;
     }
     return qutoed_state;
